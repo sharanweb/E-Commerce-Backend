8156bf961338525e3674c3c064a850127ba3b8b1
Sharan Dooganavar <91963829+sharanweb@users.noreply.github.com>
2022-06-08 09:01:20 +0530

    i

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+const usersController = require("./controllers/user.controller");
+const brandsController = require("./controllers/brand.controller");
+const productsController = require("./controllers/product.controller");
+const categoriesController = require("./controllers/categories.controller");
+
+app.use("/users", usersController);
+app.use("/brands", brandsController);
+app.use("/products", productsController);
+app.use("/categories", categoriesController);
+
+module.exports = app;
